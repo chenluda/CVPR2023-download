13ba9b7d995d3326f436e91c4370d7a4f35e0ce0
CHEN LUDA <45784833+chenluda@users.noreply.github.com>
2023-06-15 20:27:36 +0800

    Update batch_zip_files.py

--- batch_zip_files.py
+++ batch_zip_files.py
@@ -15,23 +15,25 @@ def batch_zip_files(directory, save_directory, batch_size=100):
     """
     自动打包目录下的文件，每 batch_size 个文件打包成一个 zip 文件
     """
-    # 获取目录下所有文件
-    files = os.listdir(directory)
-    files = [os.path.join(directory, file) for file in files]
-
+    # 获取目录下所有文件夹
+    folders = [os.path.join(directory, folder) for folder in os.listdir(directory) if os.path.isdir(os.path.join(directory, folder))]
     # 检查保存路径是否存在，如果不存在，则创建
     if not os.path.exists(save_directory):
         os.makedirs(save_directory)
 
-    for i in tqdm(range(0, len(files), batch_size)):
+    for i in tqdm(range(0, len(folders), batch_size)):
         # 定义zip文件名
         zip_filename = f"no{i+1}to{i+batch_size}.zip"
         zip_filepath = os.path.join(save_directory, zip_filename)
 
         # 创建zip文件并添加文件
         with zipfile.ZipFile(zip_filepath, 'w') as myzip:
-            for filepath in files[i:i+batch_size]:
-                myzip.write(filepath, arcname=os.path.basename(filepath)) # arcname为压缩文件中的名称
+            for folder in folders[i:i+batch_size]:
+                for dirpath, dirnames, filenames in os.walk(folder):
+                    for filename in filenames:
+                        filepath = os.path.join(dirpath, filename)
+                        arcname = os.path.join(os.path.basename(dirpath), filename)
+                        myzip.write(filepath, arcname=arcname)  # arcname为压缩文件中的名称
 
 if __name__ == "__main__":
     batch_zip_files('./cvpr_papers', './cvpr_papers_zip')
